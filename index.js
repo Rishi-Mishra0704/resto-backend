1b02a90ec3cddc10b0a739b31cde5395b1337528
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-29 00:43:54 +0530

    update way to post data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import { body, validationResult } from "express-validator";
+// import { body, validationResult } from "express-validator";
 import { DB_URL, PORT } from "./config.js";
 import { Meal } from "./models/meals.js"; // Import the Meal model
 import { Order } from "./models/orders.js"; // Import the Order model
@@ -41,24 +41,42 @@ app.get("/meals", async (req, res) => {
   }
 });
 
-app.post(
-  "/orders",
-  [
-    // Validate user data fields
-    body("user.name").notEmpty(),
-    body("user.street").notEmpty(),
-    body("user.postal").notEmpty().isLength({ min: 6 }),
-    body("user.city").notEmpty(),
-  ],
-  async (req, res) => {
-    try {
-      const orderData = req.body; // Assuming the order data is sent in the request body
-      const order = new Order(orderData);
-      await order.save();
-      res.status(201).json(order); // Respond with the created order
-    } catch (err) {
-      console.error("Error creating order:", err);
-      res.status(500).json({ error: "Unable to create order" });
+app.post("/orders", async (req, res) => {
+  try {
+    const orderData = req.body;
+    
+    // Validate the required fields
+    if (!orderData.user || !orderData.user.name || !orderData.user.street || !orderData.user.postal || !orderData.user.city || !orderData.orderedItems || orderData.orderedItems.length === 0) {
+      res.status(400).json({ error: "Please fill all the required fields" });
+      return;
     }
+
+    console.log("Received order data:", orderData);
+
+    // Extract the user and orderedItems data
+    const { user, orderedItems } = orderData;
+
+    // Create a new order instance
+    const order = new Order({
+      user: {
+        name: user.name,
+        street: user.street,
+        postal: user.postal,
+        city: user.city,
+      },
+      orderedItems: orderedItems,
+    });
+
+    // Log the order before saving
+    console.log("Order to be saved:", order);
+
+    // Attempt to save the order
+    await order.save();
+
+    // If successful, respond with the created order
+    res.status(201).json(order);
+  } catch (err) {
+    console.error("Error creating order:", err);
+    res.status(500).json({ error: "Unable to create order" });
   }
-);
+});
